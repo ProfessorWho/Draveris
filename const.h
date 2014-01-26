33cf9121b967048f69d5199d376035a2e4e3fce8
ProfessorWho <Optimus1313@mail.ru>
2014-01-25 18:49:56 -0800

    Create const.h
    константы

---
+++ const.h
@@ -0,0 +1,23 @@
+#include "Man.h"
+#ifndef DEF_GUARD
+
+#define DEF_GUARD
+#define	ID_MENU_NEW_GAME	WM_USER+1
+#define	ID_MENU_EXIT		WM_USER+2
+#define CREATE_MAN			WM_USER+3
+//-----------------------------------------------------------------------------------------
+#define	HORIZONTAL_LINE		10  //Horizontal lines counter
+#define	VERTICAL_LINE		25	//Vertical lines counter
+
+#define	FIGUR_COUNT			7	//Figure counter
+
+#define	SIZE_WINDOW_X		600 
+#define	SIZE_WINDOW_Y		800	
+#define	SIZE_WINDOW_STOK_X	300 //Size of glass
+#define	SIZE_WINDOW_STOK_Y	700	
+#define	SPEED				500 //speed of falling
+#define MAX_MEN				5   //Max value of men
+#define SIZE_RECT			30 //size of glass rectangle
+
+
+#endif // !1
