300ff861a49c708a8bb79b901abab2863254906e
ProfessorWho <Optimus1313@mail.ru>
2014-01-28 15:40:42 -0800

    Update draw.h

--- draw.h
+++ draw.h
@@ -1,8 +1,7 @@
-#include <Windows.h>
 #ifndef DRAW_H
 #define DRAW_H
 
-void DrawStok();
+int DrawGLScene(GLvoid);
 
 void DrawFigure();
 
@@ -10,4 +9,13 @@ void DrawPicture(HWND hwnd);
 
 void drawBlockStationary(float _x, float _y, float _r, float _g, float _b);
 
-#endif // !DRAW_H
+void cleanObject();
+
+GLvoid ReSizeGLScene(GLsizei width, GLsizei height);
+
+int InitGL(GLvoid);
+
+GLvoid LoadGLTextures();
+
+void	Repaint();
+#endif
