898890548de1f89ee1bcb377dcafa12cd12a7561
ProfessorWho <Optimus1313@mail.ru>
2014-01-28 15:42:25 -0800

    Update const.c

--- const.c
+++ const.c
@@ -4,16 +4,15 @@ HINSTANCE	g_hinstance;
 HWND		g_hwnd, g_hStok, g_hFigure; //windows
 HMENU		g_hMenu;
 char		g_bFigure[4][4], g_bNextFigur[4][4]; //Massives for figures
-int			g_iStok[HORIZONTAL_LINE][VERTICAL_LINE]; //Massive for glass
+int		g_iStok[HORIZONTAL_LINE][VERTICAL_LINE]; //Massive for glass
 POINT		g_pPozFigure;			//Position of figure at moment
-int			g_iColor, g_iFigure, g_iNextFigure, g_iScore; //color, kind, kind of next, score counter
-int			man_counter;
+int		g_iColor, g_iFigure, g_iNextFigure, g_iScore; //color, kind, kind of next, score counter
+int		man_counter;
 Man*		g_man[5]; //Massive of men
-int			Timer_id[5]; //massive for linking men and threads
+int		Timer_id[5]; //massive for linking men and threads
 HBITMAP		hBitmap;
 HGLRC		hRC;
-HDC			hDC;
-HWND		glwnd;
+HDC		hDC;
 GLuint		texture[1];
 
 int background[27][12] =
